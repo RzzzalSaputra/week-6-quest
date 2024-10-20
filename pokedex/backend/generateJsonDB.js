6b36a446ef3e9e621a8bdb9772e4c48ed04e6461
Rizal Wahyu Saputra <rizalwahyu819@gmail.com>
2024-10-20 14:53:30 +0700

    "add/fetch-data-pokemon

--- pokedex/backend/generateJsonDB.js
+++ pokedex/backend/generateJsonDB.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const { type } = require("os");
+const { config } = require("process");
 
 async function generateJsonDB() {
   // TODO: fetch data pokemon api dan buatlah JSON data sesuai dengan requirement.
@@ -8,6 +10,33 @@ async function generateJsonDB() {
   try {
     const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/?limit=100";
     const pokemonList = await fetch(pokemonApiURL).then((res)=> res.json())
+    const payload = [];
+    for (let index = 0; index < pokemonList.results.length; index++) {
+      const pokemon = pokemonList.results[index];
+      const detail = await fetch (pokemon.url).then((res) => res.json());
+      const species = await fetch (detail.species.url).then((res)=>res.json());
+      const evo = await fetch (species.evolution_chain.url).then((res)=> res.json())
+      
+      const evolutionList = [evo.chain.species.name];
+      let evolveTo = evo.chain.evolves_to[0];
+      while (evolveTo) {
+        evolutionList.push(evolveTo.species.name);
+        evolveTo = evolveTo.evolves_to[0]
+      }
+      const PokeDB = {
+        id: detail.id,
+        name: pokemon.name,
+        types: detail.types.map((typ)=> typ.type.name),
+        abilities: detail.abilities.map((abi) => abi.ability.name),
+        height: detail.height,
+        weight: detail.weight,
+        cries: detail.cries,
+        evolutionChains: evolutionList,
+      }
+      payload.push(PokeDB)
+    }
+    fs.writeFileSync("./db.json", JSON.stringify(payload, null, 2), "utf-8");
+    console.log(payload);
   } catch (error) {
     console.error(error)
   }
