ed66c3fedcc12c5d9ee3e386a17b07195dcb6c6a
Rizal Wahyu Saputra <rizalwahyu819@gmail.com>
2024-10-20 12:29:39 +0700

    "add/backend-fetch"

--- pokedex/backend/generateJsonDB.js
+++ pokedex/backend/generateJsonDB.js
@@ -5,7 +5,12 @@ async function generateJsonDB() {
   // json file bernama db.json. pastikan ketika kalian menjalankan npm run start
   // dan ketika akses url http://localhost:3000/pokemon akan muncul seluruh data
   // pokemon yang telah kalian parsing dari public api pokemon
-  const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/";
+  try {
+    const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/?limit=100";
+    const pokemonList = await fetch(pokemonApiURL).then((res)=> res.json())
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 generateJsonDB();
